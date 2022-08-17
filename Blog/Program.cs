6d8549b1c6203dcb0cb662a95ef5ad3d26341edf
Jose Zaupa <jzaupa@commandalkon.com>
2022-08-16 22:37:17 -0300

    Continuação do curso Dapper

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Models;
+using Blog.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 using System;
@@ -10,75 +11,37 @@ namespace Blog
         private const string  CONNECTION_STRING = @"Password=CaiBr12345;Persist Security Info=True;User ID = sa; Initial Catalog = Blog; Data Source = BR-JZAUPA-L03;Encrypt=False";
         static void Main(string[] args)
         {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
             //CreateUser();
-            //ReadUsers();
+            ReadUsers(connection);
+            ReadURoles(connection);
             //ReadUser();
             //UpdateUser();
-            DeleteUser();
+            //DeleteUser();
+            connection.Close();
         }
-        public static void ReadUsers()
+        public static void ReadUsers(SqlConnection connection)
         {
-            using(var connection = new SqlConnection(CONNECTION_STRING))
+            var repository = new Repository<User>(connection);
+            var users = repository.Get();
+            foreach (var user in users)
             {
-                var users = connection.GetAll<User>();
-                foreach (var user in users)
-                {
-                    Console.WriteLine(user.Name);
-                }        
-            }
+                 Console.WriteLine(user.Name);
+            }        
+           
         }
-        public static void ReadUser()
+
+        public static void ReadURoles(SqlConnection connection)
         {
-            using (var connection = new SqlConnection(CONNECTION_STRING))
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+            foreach (var role in roles)
             {
-                var user = connection.Get<User>(1);
-                Console.WriteLine(user.Name);
+                Console.WriteLine(role.Name);
             }
+
         }
-        public static void CreateUser()
-        {
-            var user = new User()
-            {
-                Bio = "8x Microsoft MVP",
-                Email = "andre@balta.io",
-                Image = "https://...",
-                Name = "André Baltieri",
-                PasswordHash = "HASH",
-                Slug = "andre-baltieri"
-            };
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Insert<User>(user);
-                Console.WriteLine("Cadastro realizado com sucesso.");
-            }
-        }
-        public static void UpdateUser()
-        {
-            var user = new User()
-            {
-                Id = 1, 
-                Bio = "Equipe | balta.io",
-                Email = "hello@balta.io",
-                Image = "https://...",
-                Name = "Equipe de suporte balta.io",
-                PasswordHash = "HASH",
-                Slug = "equipe-balta"
-            };
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                connection.Update<User>(user);
-                Console.WriteLine("Atualização realizada com sucesso.");
-            }
-        }
-        public static void DeleteUser()
-        {
-            
-            using (var connection = new SqlConnection(CONNECTION_STRING))
-            {
-                var user = connection.Get<User>(1);
-                connection.Delete<User>(user);
-                Console.WriteLine("Exclusão realizada com sucesso.");
-            }
-        }
+
     }
 }
