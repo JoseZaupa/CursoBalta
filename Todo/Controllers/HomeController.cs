6452ec01eb4a5aa41a12a11cb36b2c12775b0dec
Jose Zaupa <jzaupa@commandalkon.com>
2022-09-03 15:55:51 -0300

    Continuação curso de Asp.Net

--- Todo/Controllers/HomeController.cs
+++ Todo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Controllers
 {
@@ -6,9 +8,57 @@ namespace Todo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get()
+        public List<TodoModel> Get([FromServices] AppDbContext context)
         {
-            return "Helle World";
+            return context.Todos.ToList();
+        }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.ToList().FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post(
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+            return todo;
+        }
+
+        [HttpPut("/{int:id}")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+
+        }
+        [HttpDelete("/{int:id}")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
     }
 }
--- Todo/Program.cs
+++ Todo/Program.cs
@@ -1,6 +1,11 @@
+using Todo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello as World!");
+app.MapControllers();
 
 app.Run();
