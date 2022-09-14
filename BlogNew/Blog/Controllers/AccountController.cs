909d4fd54ac908948b80c75abfc211bbfccffe82
Jose Zaupa <jzaupa@commandalkon.com>
2022-09-13 23:39:43 -0300

    Autenticação e Autorização - quase final

--- BlogNew/Blog/Controllers/AccountController.cs
+++ BlogNew/Blog/Controllers/AccountController.cs
@@ -1,6 +1,12 @@
+using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
 using Blog.Services;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
@@ -8,24 +14,70 @@ namespace Blog.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-
-        [HttpPost("v1/login")]
-        public IActionResult Login([FromServices] TokenService tokenService)
+        [HttpPost("v1/accounts/")]
+        public async Task<IActionResult> Post(
+                [FromBody] RegisterViewModel model,
+                [FromServices] BlogDataContext context)
         {
-            var token = tokenService.GenerateToken(null);
-            return Ok(token);
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModels<string>(ModelState.GetErrors()));
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModels<dynamic>(new
+                {
+                    user = user.Email,
+                    password
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(400, new ResultViewModels<string>("05x99 - Este email já está cadastrado"));
+            }
         }
 
-        [Authorize(Roles = "user")]
-        [HttpGet("v1/user")]
-        public IActionResult GetUser() => Ok(User.Identity.Name);
+        [HttpPost("v1/account/login")]
+        public async IActionResult Login([FromBody] LoginViewNodel model,
+            [FromServices] BlogDataContext context,
+            [FromServices] TokenService tokenService)
+        {
+            if (ModelState.IsValid)
+                return BadRequest(new ResultViewModels<string>(ModelState.GetErrors()));
 
-        [Authorize(Roles = "author")]
-        [HttpGet("v1/author")]
-        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
 
-        [Authorize(Roles = "admin")]
-        [HttpGet("v1/admin")]
-        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+            if (user == null)
+                return StatusCode(401, new ResultViewModels<string>("Usuário ou senha invaválidos"));
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModels<string>("Usuário ou senha invaválidos"));
+
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModels<string>(token, null));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModels<string>("05x4 - Falha interna no servidor"));
+            }
+        }
+
     }
 }
